b0f7a9a87109f987fe84a16c56e5f14297a6e0e2
dima131212 <128320096+dima131212@users.noreply.github.com>
2025-04-15 16:28:02 +0300

    Add files via upload

---
+++ src/client/ClientResponseReceiver.java
@@ -0,0 +1,22 @@
+package client;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class ClientResponseReceiver {
+	private final ObjectInputStream in;
+
+    public ClientResponseReceiver(ObjectInputStream in) {
+        this.in = in;
+    }
+
+    public void getResponce() throws ClassNotFoundException, IOException {
+    	String responce = (String) in.readObject();
+    	System.out.println(responce);
+    }
+    
+    public Object getData() throws ClassNotFoundException, IOException {
+		Object data = (Object) in.readObject();
+    	return data;
+    }
+}
